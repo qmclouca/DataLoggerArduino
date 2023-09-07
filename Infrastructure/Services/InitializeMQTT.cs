5b566585b4993a393ed51bc32ab87f680c4a0e7e
qmclouca <qmclouca@gmail.com>
2023-09-06 23:15:20 -0300

    Adicionar arquivos de projeto.

---
+++ Infrastructure/Services/InitializeMQTT.cs
@@ -0,0 +1,12 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+
+namespace DataLoggerArduino.Infrastructure.Services
+{
+    private IMqttClient _mqttClient;
+    private IMqttClientOptions _options;
+    internal class InitializeMQTT
+    {
+    }
+}
