42cbe56daaf85763c80170dff2c3d4ad10251e6f
qmclouca <qmclouca@gmail.com>
2023-09-07 20:07:57 -0300

    Auto detection of arduinos working

--- Application/MainWindow.xaml.cs
+++ Application/MainWindow.xaml.cs
@@ -1,13 +1,16 @@
-﻿using DataLoggerArduino.Infrastructure.Services;
+﻿using DataLoggerArduino.Domain.Entities;
+using DataLoggerArduino.Infrastructure.Services;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace DataLoggerArduino
 {
     public partial class MainWindow : Window
     {
+        public static List<ArduinoDevices> _ArduinoDevicesConnected = new List<ArduinoDevices>();
         public MainWindow()
         {
-            SerialCommunications.AutodetectArduinoPort();
+            _ArduinoDevicesConnected = SerialCommunications.AutodetectArduinoPort();
             InitializeComponent();
         }
     }
