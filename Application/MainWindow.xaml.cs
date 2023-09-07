5a705057434a2b793b1f688ae9153b383ee724a1
qmclouca <qmclouca@gmail.com>
2023-09-06 23:47:34 -0300

    Serial comunications

--- Application/MainWindow.xaml.cs
+++ Application/MainWindow.xaml.cs
@@ -1,24 +1,8 @@
 ﻿using DataLoggerArduino.Infrastructure.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DataLoggerArduino
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
