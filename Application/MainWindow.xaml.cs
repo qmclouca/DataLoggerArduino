907ca9a0d602df6f14b48e2a8a26747522626afb
qmclouca <qmclouca@gmail.com>
2023-09-08 23:47:41 -0300

    parâmetros de conexão

--- Application/MainWindow.xaml.cs
+++ Application/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using DataLoggerArduino.Domain.Entities;
+using DataLoggerArduino.Domain.Enumerables;
 using DataLoggerArduino.Infrastructure.Services;
+using System.Windows.Controls;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
@@ -9,6 +11,7 @@ namespace DataLoggerArduino
     public partial class MainWindow : Window
     {
         public static List<ArduinoDevices> _ArduinoDevicesConnected = new List<ArduinoDevices>();
+        //public static List<string[]>  
         public MainWindow()
         {
             _ArduinoDevicesConnected = SerialCommunications.AutodetectArduinoPort();
@@ -27,7 +30,32 @@ namespace DataLoggerArduino
             IEnumerable<string> devices = deviceNames;
             return devices;
         }
+               
 
+        private void ListBaudRatesAvailable(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedDevice = string.Empty;
+            List<string> allowableBaudRates = new List<string>();
+            if (DeviceModelsPorts.SelectedItem != null)
+            {
+                selectedDevice = DeviceModelsPorts.SelectedItem.ToString();
+            }
+            ArduinoDevices arduinoSelected = _ArduinoDevicesConnected.FirstOrDefault(x => x.name == selectedDevice);
 
+            BaudRates.ItemsSource = BaudRatesEnum.GetValues(typeof(BaudRatesEnum))
+                                                 .Cast<BaudRatesEnum>()
+                                                 .Where(b => (int)b <= arduinoSelected.maxBaudRate)
+                                                 .Select(b => b.ToString())
+                                                 .ToList();
+        }
+
+        private void SaveConnectionParameters(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void OnClickConnectDevice(object sender, RoutedEventArgs e)
+        {
+           
+        }
     }
 }
